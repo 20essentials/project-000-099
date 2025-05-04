0a5f261cc2d989834c1dff5169b4c0e0060f75e5
DominicDev <s0l0estudi4r@gmail.com>
2025-05-04 11:37:10 +0200

    v0.0.2

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-![n1](preview/n1.avif)
+[![Foto Preview](preview/n99.avif)](https://20essentials.github.io/project-000-099)
+
+<div align="center" style="display: flex; justify-content: center;">
+  <a  href="https://github.com/20essentials/project-000-098" target="_blank">&#8592;</a>
+  &nbsp;&nbsp;
+  <a  href="https://github.com/20essentials/project-000-100" target="_blank">&#8594;</a>
+</div>
--- script.js
+++ script.js
@@ -1,26 +1,15 @@
-console.group("Reference");
-console.log(
-  "Vector",
-  "https://www.vecteezy.com/vector-art/5952107-blue-vector-seamless-background-with-diamonds"
-);
-console.log(
-  "Vite Logo",
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Vitejs-logo.svg/410px-Vitejs-logo.svg.png?20220412224743"
-);
-console.groupEnd();
-
-const $ = (el) => document.querySelector(el);
-const $wrapper = $(".wrapper");
+const $ = el => document.querySelector(el);
+const $wrapper = $('.wrapper');
 const { width, height } = $wrapper.getBoundingClientRect();
 const halfWidth = width / 2;
 const halfHeight = height / 2;
 
-window.addEventListener("mousemove", (e) => {
-  $wrapper.style.transition = ".1s linear transform";
+window.addEventListener('mousemove', e => {
+  $wrapper.style.transition = '.1s linear transform';
   const { offsetX, offsetY } = e;
 
-  let rotateX = ((offsetX - halfWidth) / halfWidth) * 10;
-  let rotateY = ((offsetY - halfHeight) / halfHeight) * 10;
+  let rotateX = ((offsetX - halfWidth) / halfWidth) * 5;
+  let rotateY = ((offsetY - halfHeight) / halfHeight) * 5;
 
   $wrapper.style.transform = `
   scale(.8)
@@ -29,8 +18,8 @@ window.addEventListener("mousemove", (e) => {
 `;
 });
 
-window.addEventListener("mouseout", (e) => {
-  $wrapper.style.transition = ".3s ease transform";
+window.addEventListener('mouseout', e => {
+  $wrapper.style.transition = '.3s ease transform';
   $wrapper.style.transform = `
   scale(.8)
   rotateY(0deg) 
